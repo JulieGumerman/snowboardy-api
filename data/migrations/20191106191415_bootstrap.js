98ada1da6bec1b50312776d2b2b09e2d06047bee
bodhibrdr <julie.gumerman@gmail.com>
2019-11-06 19:30:09 -0700

    organizational whatnots

--- data/migrations/20191106191415_bootstrap.js
+++ data/migrations/20191106191415_bootstrap.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("users", table => {
+      table.increments();
+      table.string("username").notNullable().unique();
+      table.string("password").notNullable();
+  }).createTable("mountains", table => {
+      table.increments();
+      table.string("mountain_name").notNullable();
+      table.string("nearest_town").notNullable();
+      table.text("description").notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("mountains").dropTableIfExists("users");
 };
