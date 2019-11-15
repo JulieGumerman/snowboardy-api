51a4c76e6ab7e86ccd5cb4a626f1d601ce5c0d6e
bodhibrdr <julie.gumerman@gmail.com>
2019-11-15 14:08:00 -0700

    login update perhaps

--- users/user-routes.js
+++ users/user-routes.js
@@ -22,12 +22,13 @@ userRoute.post("/register", (req, res) => {
 userRoute.post("/login", (req, res) => {
     let { username, password } = req.body; 
     Users.findBy({username})
+        .first()
         .then(user => {
             if (user && bcrypt.compareSync(password, user.password)) {
                     const token = generateToken(user);
                     res.status(200).json({
                         message: "successful login",
-                        token,
+                        token, 
                         user
                     })
             } else {
