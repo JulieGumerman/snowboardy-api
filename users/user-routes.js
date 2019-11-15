fcc9f9a2621c4609422b048c4ba696a18f10d7eb
bodhibrdr <julie.gumerman@gmail.com>
2019-11-15 13:53:54 -0700

    login works

--- users/user-routes.js
+++ users/user-routes.js
@@ -15,8 +15,28 @@ userRoute.post("/register", (req, res) => {
     user.password = hash;
 
     Users.add(user)
-        .then(saved => res.status(200).json({ message: "Success!!!"}))
+        .then(saved => res.status(200).json(saved))
         .catch(err => res.status(500).json(err.message))
+})
+
+userRoute.post("/login", (req, res) => {
+    let { username, password } = req.body; 
+    Users.findBy({username})
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                    const token = generateToken(user);
+                    res.status(200).json({
+                        message: "successful login",
+                        token,
+                        user
+                    })
+            } else {
+                res.status(401).json({message: "Oh noes!!!"})
+            }
+            })
+        .catch(err => res.status(500).json(err.message))
+
 })
 
 module.exports = userRoute;
